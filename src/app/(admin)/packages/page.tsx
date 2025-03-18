dc392f721d96887e0cfc56ac7de0d7504effbf73
Muhammad Usama <48561276+USAMA327@users.noreply.github.com>
2025-03-18 11:56:27 +0500

    Update Next Config

--- src/app/(admin)/packages/page.tsx
+++ src/app/(admin)/packages/page.tsx
@@ -148,13 +148,13 @@ const PackagesTable = () => {
                   >
                     {apiLoader ? "Loading..." : "Edit"}
                   </button>
-                  <button
+                  {/* <button
                     onClick={() => handleDelete(pkg.id!)}
                     className="bg-red-500 hover:bg-red-600 px-3 py-1 rounded"
                     disabled={apiLoader}
                   >
                     {apiLoader ? "Loading..." : "Delete"}
-                  </button>
+                  </button> */}
                 </td>
               </tr>
             ))}
